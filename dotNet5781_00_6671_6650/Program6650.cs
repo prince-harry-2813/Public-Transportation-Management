fc08baf0190ad6fdebdf25bdb055639ce7dbc3d0
yossefsteinmetz1 <57216846+yossefsteinmetz1@users.noreply.github.com>
2020-10-19 01:53:39 +0300

    add partilal class with partial method for the first project

---
+++ dotNet5781_00_6671_6650/Program6650.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace dotNet5781_00_6671_6650
+{
+    partial public class Program
+    {
+        public Program()
+        {
+            static partial void Welcome6650()
+            {
+                Console.WriteLine("I can't beleive I'm doing some work");
+            }
+        }
+    }
+}
