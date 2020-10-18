f9b8826efa2c84262bc872177f262e6d146800d3
prince-harry-2813 <54502376+prince-harry-2813@users.noreply.github.com>
2020-10-19 01:39:33 +0300

    hello world console app

---
+++ dotNet5781_00_6671_6650/Program6671.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00_6671_6650
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome6671();
+            Console.ReadKey();
+
+        }
+
+        private static void Welcome6671()
+        {
+            Console.WriteLine("enter your name");
+            var name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+        static partial void Welcome6650();
+    }
+}
