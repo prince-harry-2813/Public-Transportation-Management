285de5d1fde7c0fd43b903f24c4a9b0285a2aac9
prince-harry-2813 <54502376+prince-harry-2813@users.noreply.github.com>
2020-10-19 02:49:47 +0300

    initalize ex1

---
+++ dotNet_01_6671_6650/Bus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet_01_6671_6650
+{
+    class Bus
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
