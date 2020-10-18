1fe702f27d8cff626c461bf494c2bea5e098a119
yossefsteinmetz1 <57216846+yossefsteinmetz1@users.noreply.github.com>
2020-10-19 02:11:39 +0300

    IFxed messed up shit tht happened because of me

---
+++ dotNet5781_00_6671_6650/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00_6671_6650
+{
+    public partial class Program
+    {
+        /// <summary>
+        /// First Bla bla method 
+        /// </summary>
+        static partial void Welcome6650()
+        {
+            Console.WriteLine("Trobles in haven ");
+        }
+    }
+}
