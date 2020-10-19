69f91d51bb6c5e3d8a967e9c7be60ed95f657b84
prince-harry-2813 <54502376+prince-harry-2813@users.noreply.github.com>
2020-10-19 04:34:28 +0300

    WIP: Creating bus struture and some methodes

---
+++ dotNet_01_6671_6650/Program.cs
@@ -0,0 +1,81 @@
+﻿using dotNet_01_6671_6650.Structs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet_01_6671_6650
+{
+    class Program
+    {
+        /// <summary>
+        /// User option selector
+        /// </summary>
+        private static int option = -1;
+        public static List<Bus> BusList { get; set; }
+
+        public static bool Flag { get; set; } = true;
+
+
+        /// <summary>
+        /// Insert new sequence of a bus by licence number and first registration by mendetory and optinal properties
+        /// </summary>
+        /// <param name="licensNumber"></param>
+        /// <param name="firstRegistration"></param>
+        /// <param name="fuel"></param>
+        /// <param name="maintenence"></param>
+        /// <param name="totalKM"></param>
+        public static void InsertBus(int licensNumber, DateTime firstRegistration, int fuel = 0, int maintenence = 0, int totalKM = 0)
+        {
+            Bus bus = new Bus(licensNumber, firstRegistration, fuel, maintenence, totalKM);
+            BusList.Add(bus);
+        }
+
+        /// <summary>
+        /// Return Instance of a bus incase its exsist and can take the ride 
+        /// else return false
+        /// </summary>
+        /// <param name="licensNumber"></param>
+        /// <param name="rideRange"></param>
+        /// <returns></returns>
+        public static Bus ChooseBus(int licensNumber ,  int rideRange)
+        {
+            return (Bus)BusList.Where((b) => b.LicensNmuber == licensNumber && b.CanTakeRide(rideRange));
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="licensNumber"></param>
+        public static void RefuelAndMaintainBus(int licensNumber)
+        {
+            Bus bus = (Bus)BusList.Where((b) => b.LicensNmuber == licensNumber);
+           
+        }
+
+        public static void Main(string[] args)
+        {
+            do
+            {
+                Console.WriteLine(@"PLease Choose An Oparation :
+                                     1 : Register new Bus
+                                     2 : Choose a Bus for a ride 
+                                     3 : Refule or mantainance
+                                     4 : Display Buses KM range since last treatment
+                                     
+                                     0 : Exit");
+                int.TryParse(Console.ReadLine(), out option);
+                if (option == -1)
+                {
+                    Console.WriteLine("Wrong input, please try again");
+                }
+
+
+            } while (Flag);
+        }
+
+
+    }
+}
---
+++ dotNet_01_6671_6650/Structs/bus.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet_01_6671_6650.Structs
+{
+    public class Bus
+    {
+        /// <summary>
+        /// Date of last treatment
+        /// enter dateTime.now
+        /// </summary>
+        public DateTime LastTreatment { get; set; }
+        /// <summary>
+        /// Bus key
+        /// tow option to registration
+        /// 7 or 8 digits
+        /// </summary>
+        public int LicensNmuber { get; set; } = 0;
+        /// <summary>
+        /// Fuel status
+        /// between 0 - 1200 
+        /// </summary>
+        public int Fuel { get; set; } = 0;
+        /// <summary>
+        /// KM since last tritment
+        /// between 0 - 20,000
+        /// </summary>
+        public int Maintenance { get; set; } = 0;
+        /// <summary>
+        /// Sum of all KM since first travel
+        /// can't bre reduce
+        /// </summary>
+        public int TotalKM { get ;private set; } = 0;
+        /// <summary>
+        /// Date of the get in to service 
+        /// </summary>
+        public DateTime FirstRegistration { get; set; }
+        //
+        //
+        //
+        /// <summary>
+        /// Ctor that accept at least tow params for licens and date
+        /// if there is no other arguments initial default values.
+        /// </summary>
+        /// <param name="licensNumber"></param>
+        /// <param name="firstRegistration"></param>
+        /// <param name="fuel"></param>
+        /// <param name="maintenence"></param>
+        public Bus(int licensNumber, DateTime firstRegistration, int fuel = 0, int maintenence = 0,int totalKM=0)
+        {
+            LicensNmuber = licensNumber;
+            FirstRegistration = firstRegistration;
+            Fuel = fuel;
+            Maintenance = maintenence;
+            TotalKM = totalKM;
+        }
+
+        /// <summary>
+        /// Sets the bus toal KM and evoid decreasing its value 
+        /// </summary>
+        /// <param name="newKM"></param>
+        public void SetTotalKM(int newKM)
+        {
+            if (newKM < 0)
+                return;
+            TotalKM += newKM;
+        }
+
+
+        /// <summary>
+        /// In case There isn't enough Gas or range km since last treament return false
+        /// </summary>
+        /// <param name="rideRange"></param>
+        /// <returns></returns>
+        public bool CanTakeRide(int rideRange)
+        {
+            if (rideRange <= Fuel && Maintenance + rideRange < 20000)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
