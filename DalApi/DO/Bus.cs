bf5d77ae2486b00075f7bd6b228d94944f3eb35f
prince-harry-2813 <54502376+prince-harry-2813@users.noreply.github.com>
2020-12-28 17:28:29 +0200

    adding some entities to DO

---
+++ DalApi/DO/Bus.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DO
+{
+    public enum StatusEnum
+    {
+        Ok = 1,
+        In_Ride,
+        In_Refuling,
+        In_Maintainceing,
+        Not_Available
+    }
+    class Bus
+    {
+        public int LicenseNum { get; set; }
+        public DateTime RegisDate { get; set; }
+        public int TotalKM { get; set; }
+        public int FuelStatus { get; set; }
+        public StatusEnum Status { get; set; }
+
+    }
+}
